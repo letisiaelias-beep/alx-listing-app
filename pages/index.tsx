b82e589cae1875d30f2a1649897ebfcb0103b57f
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-08 22:58:15 +0300

    alx listing up configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Card from "../components/common/Card";
 import Button from "../components/common/Button";
-
 export default function Home() {
   return (
     <div className="p-6">
@@ -8,7 +7,7 @@ export default function Home() {
       <Card
         title="Beautiful Apartment"
         description="2 bed, 2 bath apartment in the city center"
-        imageUrl="/assets/house1.jpg"
+        imageUrl="./assets/Layout.png"
       />
       <Button label="Book Now" onClick={() => alert("Booked!")} />
     </div>
